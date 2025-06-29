a2253556c628b0eee2f769dfd1d5c4ba498443b1
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-06-29 08:17:41 +0700

    feat: Add Category entity with parent-child relationship and JPA annotations

--- src/main/java/ecommerce/entity/Category.java
+++ src/main/java/ecommerce/entity/Category.java
@@ -1,8 +1,14 @@
 package ecommerce.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,6 +31,13 @@ public class Category {
 
   @Column(name = "name", nullable = false, length = 100)
   private String name;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_id", referencedColumnName = "id")
+  private Category parent;
+
+  @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
+  private List<Category> children;
 
   @Override
   public boolean equals(Object o) {
